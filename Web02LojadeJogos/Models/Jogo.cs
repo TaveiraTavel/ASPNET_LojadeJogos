04a9d32411368b7f4d0d20a45dcd44422610d773
TaveiraTavel <mateus.staveira@gmail.com>
2021-09-22 12:11:14 -0300

    Informações da Model Jogo atualizadas. Criação da tabela tbJogo.

--- Web02LojadeJogos/Models/Jogo.cs
+++ Web02LojadeJogos/Models/Jogo.cs
@@ -20,15 +20,17 @@ namespace Web02LojadeJogos.Models
 
         [Display(Name = "Versão")]
         [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(20, MinimumLength = 1, ErrorMessage = "A versão deve ter entre 1 e 20 caracteres")]
         public string JogoVersao { get; set; }
 
         [Display(Name = "Desenvolvedor")]
         [Required(ErrorMessage = "Campo obrigatório")]
         [StringLength(100, MinimumLength = 5, ErrorMessage = "O nome da desenvolvedora deve ter entre 5 e 100 caracteres")]
-        public string JogoDesenvolvedor { get; set; }
+        public string JogoDesenvolvedora { get; set; }
 
         [Display(Name = "Gênero")]
         [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(40, MinimumLength = 5, ErrorMessage = "O gênero deve ter entre 5 e 40 caracteres")]
         public string JogoGenero { get; set; }
 
         [Display(Name = "Faixa etária")]
@@ -38,26 +40,13 @@ namespace Web02LojadeJogos.Models
 
         [Display(Name = "Plataforma")]
         [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(40, MinimumLength = 5, ErrorMessage = "A plataforma deve ter entre 5 e 40 caracteres")]
         public string JogoPlataforma { get; set; }
         
         [Display(Name = "Lançamento")]
         [Required(ErrorMessage = "Campo obrigatório")]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime JogoLancamento
-        {
-            get
-            {
-                return this.jogoLancamento.HasValue
-                    ? this.jogoLancamento.Value
-                    : DateTime.Now;
-            }
-            set
-            {
-                this.jogoLancamento = value;
-            }
-        }
-
-        private DateTime? jogoLancamento = null;
+        [Range(1000, 9999, ErrorMessage = "O ano de lançamento deve ser entre 1000 e 9999")]
+        public int JogoAnoLancamento { get; set; }
 
         [Display(Name = "Sinopse")]
         [Required(ErrorMessage = "Campo obrigatório")]
